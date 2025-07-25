e4824db6a13cc7a1904696ba066057cae81f56f2
anonymousecode <anonymousecode1@gmail.com>
2025-07-25 20:48:10 +0530

    changed mysql to mongodb, changed env, updated models and controllers

--- app/Models/Diarynote.php
+++ app/Models/Diarynote.php
@@ -2,12 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model; // Base model for MongoDB
 
 class Diarynote extends Model
 {
-    //
-    public $timestamps = false;
+    
+    protected $connection = 'mongodb';
+    protected $collection = 'diarynotes';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'tag',
+        'email',
+        'day',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [];
 
 
 }
